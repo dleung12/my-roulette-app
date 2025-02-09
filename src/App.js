93c97152f509dfc2b1cf26928ed270aee947ac90
dleung12 <desperky2@gmail.com>
2025-02-09 23:18:38 +1100

    Revert filepath to earlier version

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RouletteTracker from './Roulette';
+
+function App() {
+  return (
+    <div className="App">
+      <RouletteTracker />
+    </div>
+  );
+}
+
+export default App;
